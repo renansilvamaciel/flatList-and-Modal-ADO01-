fe9337d8d4bbcf3e3dbd327c1e415012416f080b
Renan Silva Maciel <renan.tecnology@gmail.com>
2021-09-14 21:57:09 -0300

    Add files via upload
    Código JS uso de Modal e FlatList com React

---
+++ ado_01/App.js
@@ -0,0 +1,389 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, FlatList, Pressable, Image,Modal } from 'react-native';
+import Constants from 'expo-constants';
+
+
+const ShowDetalhes = ({display,toogleModal,mensagem}) => (   
+    <Modal
+          animationType="slide"
+          transparent={true}
+          visible={display}
+          onRequestClose={toogleModal}
+    >
+
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+                <Pressable onPress={toogleModal}>
+                  <Text>{mensagem}</Text>
+                </Pressable>
+          </View>
+        </View>
+    
+    </Modal>
+        
+ )
+
+const Pessoa = ({nome,email,link,username}) => {
+
+    //state para controle do Modal
+    const [modal,setModal] = React.useState(false)
+
+    function mudaModal(){
+      setModal(!modal)
+    }
+
+    return(
+    <View>
+      <ShowDetalhes display={modal} toogleModal={mudaModal} mensagem={username}/>
+      
+      <Pressable onPress={mudaModal}>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: link,
+          }}
+        />
+
+        <Text style={styles.paragraph}>{nome}</Text>
+      </Pressable>
+    </View>
+    )
+}
+
+
+const DATA = [
+       {
+    "id": 1,
+    "first_name": "Leanne",
+    "last_name": "Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "avatar": "https://i.pinimg.com/564x/6f/1e/fb/6f1efb3e2f7ddb6f6b9a3dbefabe0c67.jpg",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "first_name": "Ervin ",
+    "last_name": "Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+     "avatar":"https://i.pinimg.com/564x/c8/59/94/c85994a348341cddaf868d72c8722e54.jpg",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  },
+  {
+    "id": 3,
+    "first_name": "Clementine",
+    "last_name": "Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "avatar":"https://i.pinimg.com/564x/65/dd/b9/65ddb9b10b0491b324285ffe0e1bb899.jpg",
+    "address": {
+      "street": "Douglas Extension",
+      "suite": "Suite 847",
+      "city": "McKenziehaven",
+      "zipcode": "59590-4157",
+      "geo": {
+        "lat": "-68.6102",
+        "lng": "-47.0653"
+      }
+    },
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "company": {
+      "name": "Romaguera-Jacobson",
+      "catchPhrase": "Face to face bifurcated interface",
+      "bs": "e-enable strategic applications"
+    }
+  },
+  {
+    "id": 4,
+    "first_name": "Patricia",
+    "last_name": "Lebsack",
+    "username": "Karianne",
+    "email": "Julianne.OConner@kory.org",
+    "avatar":"https://i.pinimg.com/564x/64/0f/ec/640fecb1e3bca0bcf2092207033de79d.jpg",
+    "address": {
+      "street": "Hoeger Mall",
+      "suite": "Apt. 692",
+      "city": "South Elvis",
+      "zipcode": "53919-4257",
+      "geo": {
+        "lat": "29.4572",
+        "lng": "-164.2990"
+      }
+    },
+    "phone": "493-170-9623 x156",
+    "website": "kale.biz",
+    "company": {
+      "name": "Robel-Corkery",
+      "catchPhrase": "Multi-tiered zero tolerance productivity",
+      "bs": "transition cutting-edge web services"
+    }
+  },
+  {
+    "id": 5,
+    "first_name": "Chelsey",
+    "last_name": "Dietrich",
+    "username": "Kamren",
+    "email": "Lucio_Hettinger@annie.ca",
+    "avatar":"https://i.pinimg.com/564x/17/6f/74/176f74b37d78319f6e580a772b2b05b3.jpg",
+    "address": {
+      "street": "Skiles Walks",
+      "suite": "Suite 351",
+      "city": "Roscoeview",
+      "zipcode": "33263",
+      "geo": {
+        "lat": "-31.8129",
+        "lng": "62.5342"
+      }
+    },
+    "phone": "(254)954-1289",
+    "website": "demarco.info",
+    "company": {
+      "name": "Keebler LLC",
+      "catchPhrase": "User-centric fault-tolerant solution",
+      "bs": "revolutionize end-to-end systems"
+    }
+  },
+  {
+    "id": 6,
+    "first_name": "Mrs. Dennis",
+    "last_name": "Schulist",
+    "username": "Leopoldo_Corkery",
+    "email": "Karley_Dach@jasper.info",
+    "avatar":"https://i.pinimg.com/564x/07/f4/61/07f461718127dda3e6acf5b788293b1f.jpg",
+    "address": {
+      "street": "Norberto Crossing",
+      "suite": "Apt. 950",
+      "city": "South Christy",
+      "zipcode": "23505-1337",
+      "geo": {
+        "lat": "-71.4197",
+        "lng": "71.7478"
+      }
+    },
+    "phone": "1-477-935-8478 x6430",
+    "website": "ola.org",
+    "company": {
+      "name": "Considine-Lockman",
+      "catchPhrase": "Synchronised bottom-line interface",
+      "bs": "e-enable innovative applications"
+    }
+  },
+  {
+    "id": 7,
+    "first_name": "Kurtis",
+    "last_name": "Weissnat",
+    "username": "Elwyn.Skiles",
+    "email": "Telly.Hoeger@billy.biz",
+    "avatar":"https://i.pinimg.com/564x/d9/51/08/d95108ca695e21bbace5bd832e48dcfd.jpg",
+    "address": {
+      "street": "Rex Trail",
+      "suite": "Suite 280",
+      "city": "Howemouth",
+      "zipcode": "58804-1099",
+      "geo": {
+        "lat": "24.8918",
+        "lng": "21.8984"
+      }
+    },
+    "phone": "210.067.6132",
+    "website": "elvis.io",
+    "company": {
+      "name": "Johns Group",
+      "catchPhrase": "Configurable multimedia task-force",
+      "bs": "generate enterprise e-tailers"
+    }
+  },
+  {
+    "id": 8,
+    "first_name": "Nicholas",
+    "last_name": "Runolfsdottir V",
+    "username": "Maxime_Nienow",
+    "email": "Sherwood@rosamond.me",
+    "avatar":"https://i.pinimg.com/564x/5f/d2/f6/5fd2f68474fc8deb983d149ace53190d.jpg",
+    "address": {
+      "street": "Ellsworth Summit",
+      "suite": "Suite 729",
+      "city": "Aliyaview",
+      "zipcode": "45169",
+      "geo": {
+        "lat": "-14.3990",
+        "lng": "-120.7677"
+      }
+    },
+    "phone": "586.493.6943 x140",
+    "website": "jacynthe.com",
+    "company": {
+      "name": "Abernathy Group",
+      "catchPhrase": "Implemented secondary concept",
+      "bs": "e-enable extensible e-tailers"
+    }
+  },
+  {
+    "id": 9,
+    "first_name": "Glenna",
+    "last_name": "Reichert",
+    "username": "Delphine",
+    "email": "Chaim_McDermott@dana.io",
+    "avatar":"https://i.pinimg.com/564x/27/b7/3e/27b73e5aa26cf776df4777768a0924e2.jpg",
+    "address": {
+      "street": "Dayna Park",
+      "suite": "Suite 449",
+      "city": "Bartholomebury",
+      "zipcode": "76495-3109",
+      "geo": {
+        "lat": "24.6463",
+        "lng": "-168.8889"
+      }
+    },
+    "phone": "(775)976-6794 x41206",
+    "website": "conrad.com",
+    "company": {
+      "name": "Yost and Sons",
+      "catchPhrase": "Switchable contextually-based project",
+      "bs": "aggregate real-time technologies"
+    }
+  },
+  {
+    "id": 10,
+    "first_name": "Clementina",
+    "last_name": "DuBuque",
+    "username": "Moriah.Stanton",
+    "email": "Rey.Padberg@karina.biz",
+    "avatar":"https://i.pinimg.com/564x/3b/24/4d/3b244d33f2fc55485dab1399e4b5950c.jpg",
+    "address": {
+      "street": "Kattie Turnpike",
+      "suite": "Suite 198",
+      "city": "Lebsackbury",
+      "zipcode": "31428-2261",
+      "geo": {
+        "lat": "-38.2386",
+        "lng": "57.2232"
+      }
+    },
+    "phone": "024-648-3804",
+    "website": "ambrose.net",
+    "company": {
+      "name": "Hoeger LLC",
+      "catchPhrase": "Centralized empowering task-force",
+      "bs": "target end-to-end models"
+    }
+  }
+    ];
+
+
+
+//item com uma arrow function
+/*const meuItemObj = ({item}) => (
+  <View>
+      <Text style={styles.paragraph}>{item.title}</Text>
+    </View>
+)*/
+
+
+
+export default function App() {
+
+  //função que renderiza cada item do FlatList
+  function meuItem({item}){
+    let nomeCompleto = item.first_name + " " + item.last_name
+    
+    return(
+      <Pessoa nome={nomeCompleto} 
+              link={item.avatar}
+              email={item.email}
+              username={item.username}
+      />
+    )
+  }
+  
+
+  return (
+
+    <View style={styles.container}>
+
+      <FlatList
+        data={DATA}
+        renderItem={meuItem}
+        keyExtractor={item => item.id}
+      />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 12,
+    padding: 12,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'pink'
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    alignSelf: 'center'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
